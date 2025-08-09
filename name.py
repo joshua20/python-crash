65d469abd028ffaeb6d5f5f5017038acca892d61
joshua <jnyakundi31@yahoo.com>
2025-08-09 14:38:19 +0330

    changing case in strings

---
+++ name.py
@@ -0,0 +1,16 @@
+#changing case in a string with methods
+
+name="joshua nyakundi"
+#print the name in title case- all words start in caps
+print(f"{name.title()}-title() case")
+
+#print name in caps -upper()
+
+print(f"{name.upper()}-upper() case")
+
+#print the name back small letters- lower()
+
+print(f"{name.lower()}-lower case")
+
+
+
