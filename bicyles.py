6061a4af1e5e4ad74e808dfa77ad67e8200a8dd3
joshua <jnyakundi31@yahoo.com>
2025-08-09 21:17:33 +0330

    accessing list elements

--- bicyles.py
+++ bicyles.py
@@ -3,3 +3,14 @@
 bicyles=['trek','commondale','redline', 'specialized']
 
 print(bicyles)
+
+#accessing elements in a list
+
+print(bicyles[0])
+
+#change case of list element
+
+print(bicyles[0].title())
+print(bicyles[1].upper())
+print(bicyles[2])
+print(bicyles[3].lower())
