019fafc8cb9dd2466e82662b9a9535012f7f16e8
joshua <jnyakundi31@yahoo.com>
2025-08-09 21:45:55 +0330

    example

---
+++ names.py
@@ -0,0 +1,24 @@
+names=['john','branson','manu','eliel']
+
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+
+
+#print greetings for each name
+
+print(f"hello, {names[0]}")
+
+print(f"hello, {names[1]}")
+
+print(f"hello, {names[2]}")
+
+print(f"hello, {names[3]}")
+
+cars=["honda","toyota","suzuki"]
+
+car_message=f"i love {cars[2]} cars"
+
+print(car_message)
+
