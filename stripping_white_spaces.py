95e5c4432aeeb267460fc3458d05ad13a0d8a04a
joshua <jnyakundi31@yahoo.com>
2025-08-09 19:15:56 +0330

    stripping white spaces

---
+++ stripping_white_spaces.py
@@ -0,0 +1,23 @@
+favorite_language="python "
+print(favorite_language)
+
+#rstrip()
+
+corrected_language=favorite_language.rstrip()
+
+print(f"corrected favorite language: {corrected_language}")
+
+#lstrip()
+
+my_language="  kiswahili"
+
+lstrip_language=my_language.lstrip()
+
+print(f"left stripped: {lstrip_language}")
+
+#left and right strip
+
+message="  kenya inahitaji rahisi wa kudumu  "
+message=message.strip()
+
+print(message)
