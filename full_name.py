408cc947fa2f4c748ec00895d5aef65ac3154973
joshua <jnyakundi31@yahoo.com>
2025-08-09 18:46:28 +0330

    using variables in strings

---
+++ full_name.py
@@ -0,0 +1,9 @@
+first_name="joshua"
+
+print(f"firstname = {first_name}")
+last_name="Nyakundi"
+print(f"last_name= {last_name}")
+
+full_name= f"{first_name} {last_name}"
+
+print(f"Hello, { full_name}")
