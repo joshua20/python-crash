6746c3b07b68c63c36de367b79242582cdd039cc
joshua <jnyakundi31@yahoo.com>
2025-08-09 19:01:38 +0330

    white spaces

---
+++ white_space.py
@@ -0,0 +1,14 @@
+#adding white spaces in strings
+
+#using tabs
+print("\t Pyhton")
+
+
+#adding a new line
+
+print("languages: \nPython \nC \nJavascript")
+
+
+#using tab and new line
+
+print("\n\tPython\n\tC  \n\tJavascript")
