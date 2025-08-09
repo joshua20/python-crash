f8fd91769832f4d43a9b7ceb6e03aaa1c1ddeb0a
joshua <jnyakundi31@yahoo.com>
2025-08-09 22:10:20 +0330

    modifying elements in a list

---
+++ motorcycles.py
@@ -0,0 +1,12 @@
+
+#modifying elements in a list
+
+motorcycles=['honda','yamaha','suzuki']
+
+print(motorcycles)
+
+motorcycles[0]="ducati"
+
+print(f"new motorcycles list: \n\t {motorcycles}")
+
+
