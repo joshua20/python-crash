793ca592bafb77d435140123264a8a123855a04d
joshua <jnyakundi31@yahoo.com>
2025-08-09 21:33:57 +0330

    using individual list values

--- bicyles.py
+++ bicyles.py
@@ -14,3 +14,11 @@ print(bicyles[0].title())
 print(bicyles[1].upper())
 print(bicyles[2])
 print(bicyles[3].lower())
+
+#using indidual values from a list
+
+message=f"my firste bicyle was a {bicyles[0].title()}"
+
+print(message)
+
+
