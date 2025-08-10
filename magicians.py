6178edc3f935661da64582c884ccbd151cfa6e74
joshua <jnyakundi31@yahoo.com>
2025-08-10 23:09:05 +0330

    simple for loop

---
+++ magicians.py
@@ -0,0 +1,7 @@
+#using looping to access list elements
+
+magicians=['alice','davis','carolina']
+
+print("These are the magicians in the list :")
+for magician in magicians:
+    print(f"\n\t {magician.title()}")
---
+++ pizza.py
@@ -0,0 +1,10 @@
+pizzas=["peperoni","chicken","caramen"]
+
+for pizza in pizzas:
+    print(f"i love {pizza} pizza")
+
+print(f"i really love {pizzas[0]}")
+
+print(f"i really love {pizzas[1]}")
+print(f"i really love {pizzas[2]}")
+print("I really love Pizza!")
