c26581a357997ba215065567297da5f120cd10fa
joshua <jnyakundi31@yahoo.com>
2025-08-10 22:39:03 +0330

    sorting elements in a list

---
+++ cars.py
@@ -0,0 +1,14 @@
+#sorting a list permanently with sort()
+
+cars=['bmw','audi','toyota','subaru']
+print(cars)
+cars.sort()
+
+print(cars)
+
+#reverse sorting
+
+cars.sort(reverse=True)
+
+print(f"reverse sorted cars list :\n\t {cars}")
+
