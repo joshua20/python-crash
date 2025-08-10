57af3c1d86deddae6179106fa03165042f81d028
joshua <jnyakundi31@yahoo.com>
2025-08-11 00:39:15 +0330

    looping through a slice

--- player.py
+++ player.py
@@ -8,4 +8,8 @@ print(f"3 players:\n\t {players[0:3]}")
 
 print(f"player 2 to 4: \n\t {players[1:4]}")
 
+#looping through a slice
 
+print("these are the first 3 p;ayers from the my team")
+for player in players[:3]:
+    print( player)
