c4a9929f3c34f776d060ea2440d447388f690d41
joshua <jnyakundi31@yahoo.com>
2025-08-10 23:53:23 +0330

    simple statistics with lists

---
+++ statistics.py
@@ -0,0 +1,7 @@
+#a simple statistics with List of numbers
+digits=list(range(1,101))
+print(digits)
+print(f"minimum: {min(digits)}")
+print(f"Maxmum: {max(digits)}")
+
+print(f"the sum: {sum(digits)}")
