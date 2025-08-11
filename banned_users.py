2135785058a4281c519c96aadd7f416b3e5ce6f3
joshua <jnyakundi31@yahoo.com>
2025-08-11 23:28:33 +0330

    checking if value not in a List

---
+++ banned_users.py
@@ -0,0 +1,12 @@
+#checking if value not in a list
+
+banned_users=['andrew','kimeu','carolina','david']
+
+print(banned_users)
+
+user="joshua"
+
+answer=user not in banned_users
+
+print(f" {user.title()} not in {banned_users}")
+print(answer)
