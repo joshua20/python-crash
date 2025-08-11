9c22254c3547ff58e234b9db78e946692b4d13c4
joshua <jnyakundi31@yahoo.com>
2025-08-11 23:19:10 +0330

    checking if value in a list

---
+++ magic_numbers.py
@@ -0,0 +1,24 @@
+# answ
+answer=17
+
+if answer != 42:
+    print("this is not te correct answer.please try again!")
+
+
+age=19
+age<21
+age>21
+age>=21
+
+#checking whether a value is in a List
+
+toppings=['mushrooms','onion','pineaple']
+print("toppings")
+print(toppings)
+ask= 'mushrooms' in toppings
+
+print(f"mushrooms in toppings: \n\t {ask}")
+
+peperoni='peperoni' in toppings
+
+print(f"peperoni in toppings :\n\t {peperoni}")
