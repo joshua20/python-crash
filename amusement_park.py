421d63079761a8908db3c9c1091e055efcf19fef
joshua <jnyakundi31@yahoo.com>
2025-08-11 23:40:34 +0330

    if-elif-else chain

---
+++ amusement_park.py
@@ -0,0 +1,13 @@
+#if-elif-else chain
+
+age= 12
+
+if age ==4 :
+    print("your admission cost is 0")
+
+elif age <18:
+    print("your admission cost is 25")
+else:
+    print("your admission is 40")
+
+
