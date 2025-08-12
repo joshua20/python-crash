2ca3755ce2add372a44767b5417862b622540339
joshua <jnyakundi31@yahoo.com>
2025-08-12 22:18:41 +0300

    adding new key-value pairs to a dictionary

--- alien.py
+++ alien.py
@@ -16,3 +16,11 @@ print(alien_0['points'])
 new_points=alien_0['points']
 
 print(f"you just earned: {new_points} points!")
+
+#adding new key-value pairs
+
+alien_0['x_position']=0
+alien_0['y_position']=25
+
+print(f"the new alien_0: \n\t {alien_0}")
+
