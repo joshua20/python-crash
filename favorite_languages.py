e580c42987b580e3a335b09c5c53653aab2ea81b
joshua <jnyakundi31@yahoo.com>
2025-08-12 23:08:40 +0300

    a dictionary of similar objects

---
+++ favorite_languages.py
@@ -0,0 +1,16 @@
+#a dictionary of similar objects
+
+favorite_languages={
+        'jane':'Python',
+        'sarah':'C',
+        'edwin':'Rust',
+        'james':'Ruby'
+        }
+
+language=favorite_languages['sarah'].title()
+
+print(f"Sarah' favorite language is {language}")
+
+print(favorite_languages['sarah'])
+
+
