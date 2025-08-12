17d57ab5c75fe8b6c633e340d9835b7429747cd1
joshua <jnyakundi31@yahoo.com>
2025-08-12 23:37:30 +0300

    chapter 6 exercise

---
+++ chapter6.py
@@ -0,0 +1,20 @@
+#create a person you know
+
+branson={
+        'firstname':'branson',
+        'lastname':'nyakundi',
+        'age':9,
+        'city':'nairobi',
+}
+
+print(f" The person i know is {branson['firstname']} {branson['lastname']}")
+print(f"{branson['firstname']} is aged {branson['age']} and lives in {branson['city']}")
+
+
+favorite_numbers={
+            'jose':17,
+            'div':10,
+            'manu':11,
+        }
+
+print(f"these are their  favorite numbers: \n\t{favorite_numbers['jose']}, \n\t {favorite_numbers['div']} \n\t {favorite_numbers['manu']}")
