8fc066658222b97d57d8700f03a4cc2f5f2b5e3a
joshua <jnyakundi31@yahoo.com>
2025-08-12 23:15:02 +0300

    getting values from dicts using get() method

--- favorite_languages.py
+++ favorite_languages.py
@@ -13,4 +13,11 @@ print(f"Sarah' favorite language is {language}")
 
 print(favorite_languages['sarah'])
 
+#using the get() to extract values from dictionaries
+
+language_value=favorite_languages.get('javascript','langauge does not exist')
+
+print("getting values from dicts using the get()")
+print(language_value)
+
 
