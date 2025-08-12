c7108cde5be19741c2606bb886c7544e221a8deb
joshua <jnyakundi31@yahoo.com>
2025-08-12 22:13:34 +0300

    simple ductionary

---
+++ alien.py
@@ -0,0 +1,18 @@
+#a simple dictionary
+
+alien_0={
+
+        'color':'green',
+        'points':5,
+
+        }
+print(alien_0)
+
+print(alien_0['color'])
+print(alien_0['points'])
+
+#incase you kill the alien you gain 5 points
+
+new_points=alien_0['points']
+
+print(f"you just earned: {new_points} points!")
