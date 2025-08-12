205e98f9fd4255e29a7270655fef316f7f45fb2a
joshua <jnyakundi31@yahoo.com>
2025-08-12 23:42:21 +0300

    looping for key and value

---
+++ user.py
@@ -0,0 +1,12 @@
+#looping  through a dictionary
+
+user_0={
+        'username':'josh20',
+        'firstname':'Joshua',
+        'Lastname':'Ariemba',
+        }
+
+for key, value in user_0.items():
+    print(f"{key}:{value}")
+
+
