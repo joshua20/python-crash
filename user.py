c641836b3245c82a0e96e960489769c42987c814
joshua <jnyakundi31@yahoo.com>
2025-08-12 23:59:25 +0300

    looping through a dict's values only

--- user.py
+++ user.py
@@ -10,3 +10,19 @@ for key, value in user_0.items():
     print(f"{key}:{value}")
 
 
+#looping for keys in a dictionary
+print("looping through a dictionary for keys only")
+for key in user_0.keys():
+    print(key)
+
+#looping through a dict 's keys in a particular order
+
+for key in sorted(user_0.keys()):
+    print(f"i now know your keys\n\t {key}")
+
+#looping through dict's values 
+
+print("the user has the following detais")
+
+for values in user_0.values():
+    print(values)
