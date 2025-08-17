dd65e8349796860df03a24101189af3040554449
joshua <jnyakundi31@yahoo.com>
2025-08-17 22:35:47 +0300

    print the toppings

--- functions/pizza.py
+++ functions/pizza.py
@@ -2,7 +2,9 @@
 
 def make_pizza(*toppings):
     '''print a list of arbitrary toppings that have been requested'''
-    print(toppings)
+    print("\n making pizza wit the following toppings")
+    for topping in toppings:
+        print(f"- {topping}")
 
 make_pizza('peperoni')
 make_pizza('mushrooms','green peppers','extra cheese')
