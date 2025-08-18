6ff8873e2016271150b41a32bf3f6580d531bbc6
joshua <jnyakundi31@yahoo.com>
2025-08-18 23:15:25 +0300

    accessing a file's lines

--- files/file_reader.py
+++ files/file_reader.py
@@ -3,3 +3,11 @@ from pathlib import Path
 path=Path('pi_digits.txt')
 contents=path.read_text()
 print(contents)
+
+
+#reading lines
+
+lines=contents.splitlines()
+
+for line in lines:
+    print(line)
