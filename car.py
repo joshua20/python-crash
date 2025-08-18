b9908107d68b28d4dc71a9e7c07caf3e6b725f2f
joshua <jnyakundi31@yahoo.com>
2025-08-18 11:37:56 +0300

    modifying an attributes value directly

--- car.py
+++ car.py
@@ -6,12 +6,29 @@ class Car:
         self.make=make
         self.model=model
         self.year=year
+        self.odometer_reading=0
 
     def get_descriptive_name(self):
         '''return a neatly formatted name .'''
         long_name = f"{self.year} {self.make} {self.model}"
         return long_name.title()
+    
+    def read_odometer(self):
+        '''method to read car's odometer'''
+        print(f"This car has {self.odometer_reading} miles on it.")
 
 
 my_new_car=Car('audi','a4',2012)
 print(my_new_car.get_descriptive_name())
+
+#modifying an attribute directly
+print("cars milage before start of journey")
+my_new_car.read_odometer()
+
+print("new odometer reading")
+
+my_new_car.odometer_reading=40
+
+my_new_car.read_odometer()
+
+
