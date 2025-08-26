c98cd75b5a0e8794f7570d18322dd41159bde02b
joshua <jnyakundi31@yahoo.com>
2025-08-26 23:09:50 +0300

    writing multiple lines to a file

--- files/write_message.py
+++ files/write_message.py
@@ -6,4 +6,12 @@ path=Path('programming.txt')
 
 path.write_text("I love programming in Python")
 
+#writing multiple lines
+
+contents="i feel python is the future for me\n"
+contents+="it really makes programming fun\n"
+contents+="you should take keen interest into programming\n"
+contents+="i will keep up my pace"
+path2=Path('programming.txt')
+path.write_text(contents)
 
