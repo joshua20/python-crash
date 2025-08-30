b213eb77c42636f47a4884e2e01de1f03c315ff6
joshua <jnyakundi31@yahoo.com>
2025-08-30 12:09:33 +0300

    test failed due to modified name_function

--- tests/name_function.py
+++ tests/name_function.py
@@ -1,8 +1,8 @@
 #create test function
 
 
-def get_formatted_name(first,last):
+def get_formatted_name(first,middle,last):
     '''function to generate a formatted name'''
-    full_name=f"{first} {last}"
+    full_name=f"{first} {middle} {last}"
     return full_name.title()
 
