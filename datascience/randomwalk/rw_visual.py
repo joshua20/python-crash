42ef9a2a113eb4470801ca0058286c4440a8b7fe
joshua <jnyakundi31@yahoo.com>
2025-09-20 23:28:09 +0300

    coloring the points

--- datascience/randomwalk/rw_visual.py
+++ datascience/randomwalk/rw_visual.py
@@ -17,8 +17,11 @@ while True:
     plt.style.use('classic')
 
     fig,ax=plt.subplots()
-
-    ax.scatter(rw.x_values,rw.y_values,s=15)
+    
+    #coloring the points
+    point_numbers=range(rw.num_points)
+    
+    ax.scatter(rw.x_values,rw.y_values,c=point_numbers, cmap=plt.cm.Blues,edgecolors='none', s=15)
 
     ax.set_aspect('equal')
 
