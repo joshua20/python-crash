c46f528dc54be24ac22a3839c93743e0f0914471
joshua <jnyakundi31@yahoo.com>
2025-09-20 19:06:43 +0300

    styling figures using built in styles

--- datascience/mpl_squares.py
+++ datascience/mpl_squares.py
@@ -2,10 +2,13 @@
 
 import matplotlib.pyplot as plt
 
+input_values=[1,2,3,4,5,6]
 squares=[1,4,9,16,25,36]
 
+#insert inbuilt styles
+plt.style.use('bmh')
 fig,ax=plt.subplots()
-ax.plot(squares, linewidth=3)
+ax.plot(input_values,squares, linewidth=3)
 
 #set chart title and label the axes
 ax.set_title("Square Numbers", fontsize=24)
