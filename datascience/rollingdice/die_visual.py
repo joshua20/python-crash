4d8d27de81f4013dcd85c5533ade4ad29e29e8c1
joshua <jnyakundi31@yahoo.com>
2025-09-21 21:08:36 +0300

    analyzing the results

--- datascience/rollingdice/die_visual.py
+++ datascience/rollingdice/die_visual.py
@@ -15,3 +15,15 @@ for roll_num in range(100):
     results.append(result)
 
 print(results)
+
+#analyze the results
+
+frequencies=[]
+
+poss_results=range(1, die.num_sides +1)
+for value in poss_results:
+    frequency=results.count(value)
+    frequencies.append(frequency)
+
+print("frequencies")
+print(frequencies)
