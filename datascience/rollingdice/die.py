e3bfe02a0a451a2926095ab3d98a5ad6f1e9d6ef
joshua <jnyakundi31@yahoo.com>
2025-09-21 20:32:15 +0300

    creating a die class

---
+++ datascience/rollingdice/die.py
@@ -0,0 +1,15 @@
+#creating the die class
+
+from random import randint
+
+class Die:
+    ''' a class representing a single die'''
+
+    def __init__(self, num_sides=6):
+        '''assume a six sided die'''
+        self.num_sides=num_sides
+
+    def roll(self):
+        '''return any random number from any of the sides'''
+        return randint(1, self,num_sides)
+
