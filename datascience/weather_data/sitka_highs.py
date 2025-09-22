6046622ffe36dc1cc1c0d9de77790e2cc1cdaa53
joshua <jnyakundi31@yahoo.com>
2025-09-22 13:53:06 +0300

    extracting elevation

--- datascience/weather_data/sitka_highs.py
+++ datascience/weather_data/sitka_highs.py
@@ -9,3 +9,17 @@ reader=csv.reader(lines)
 header_row=next(reader)
 
 print(header_row)
+
+#printing the headers and their positions 
+
+for index, column_header in enumerate(header_row):
+    print(index, column_header)
+
+#extracting the elevation
+
+highs=[]
+
+for row in reader:
+    high=row[3]
+    highs.append(high)
+print(highs)
