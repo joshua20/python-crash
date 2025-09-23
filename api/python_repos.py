481021d1813a8d9081c76a070767ba53a32c67e1
joshua <jnyakundi31@yahoo.com>
2025-09-23 15:24:28 +0300

    working with response dictionary

--- api/python_repos.py
+++ api/python_repos.py
@@ -16,3 +16,22 @@ response_dict=r.json()
 #process  the results
 
 print(response_dict.keys())
+
+
+print(f"total repositories: {response_dict['total_count']}")
+print(f"Complete results: {not response_dict['incomplete_results']}")
+
+#explore information about the repositories
+
+repo_dicts=response_dict['items']
+print(f"repositories returned: {len(repo_dicts)}")
+
+#examine the first repository
+repo_dict=repo_dicts[0]
+
+print(f"\nKeys: {len(repo_dict)}")
+
+for key in sorted(repo_dict.keys()):
+    print(key)
+
+
