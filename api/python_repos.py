23487e0afe918380868b9b37754f6a6247cf42b8
joshua <jnyakundi31@yahoo.com>
2025-09-24 15:20:48 +0300

    api visualization with plotly

--- api/python_repos.py
+++ api/python_repos.py
@@ -1,5 +1,5 @@
 import requests
-
+import plotly.express as px
 #make an api call and check the response
 
 url="https://api.github.com/search/repositories"
@@ -34,4 +34,17 @@ print(f"\nKeys: {len(repo_dict)}")
 for key in sorted(repo_dict.keys()):
     print(key)
 
+#process repositry information
+repo_names,stars=[],[]
 
+for repo_dict in repo_dicts:
+
+    repo_names.append(repo_dict['name'])
+    stars.append(repo_dict['stargazers_count'])
+print(repo_names)
+print(stars)
+#make visualization
+labels={'x':"repositories", "y":"stars"}
+title="Most-starred projects on Github"
+fig=px.bar(x=repo_names, y=stars,labels=labels,title=title)
+fig.write_html("repo_names.html")
